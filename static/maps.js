b3c80863437b49ddfe575c18d9551baa6877dc57
elena-dan-at-892988686872 <e.rathsponce@gmail.com>
2023-01-22 17:17:30 +0100

    alert empty field (add cache)

--- static/maps.js
+++ static/maps.js
@@ -92,9 +92,15 @@ function addCache() {
     gameMap2.on('click', function (event) {
         if (cacheCount < 10) {
             var cacheName = prompt("Please enter the cache name:", "");
-            while (cacheName == null) { var cacheName = prompt("Please enter the cache name:", ""); }
+            if (cacheName == null || cacheName == "") {
+                alert("Cache name cannot be empty");
+                return;
+            }
             var cacheHint = prompt("Please enter the hint:", "");
-            while (cacheName == null) { var cacheName = prompt("Please enter the hint:", ""); }
+            if (cacheHint == null || cacheHint == "") {
+                alert("Cache hint cannot be empty");
+                return;
+            }
             var cache = {
                 location: event.coordinate,
                 name: cacheName,
